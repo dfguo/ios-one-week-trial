044eb36864fe49a9d10b6ea63a8640b6538b0e5b
Dafeng Guo <dfguo.joe@gmail.com>
2014-03-11 20:51:07 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IosOneWeekTrial::Application.config.secret_key_base = 'c1c0f1d6aa75fd745172f7dfbea1933f41126e8bd287fc40385df941bdaa68c44c3ee3d59d7470f35118583f51509899430a2935ffc212507daa7a22fd008d1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IosOneWeekTrial::Application.config.session_store :cookie_store, key: '_ios-one-week-trial_session'
